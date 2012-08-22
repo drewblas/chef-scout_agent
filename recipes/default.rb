ad4b974cfa88f32cb3a6aabe5e2d3597882bf376
Drew Blas <drew.blas@gmail.com>
2012-08-22 14:31:12 -0500

    Use the correct location of the crontab file

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,13 +22,20 @@ end
 
 if node[:scout_agent][:key]
   # initialize scout gem
+  crontab_path = case node.platform
+  when 'debian','ubuntu'
+    "/var/spool/cron/crontabs/#{node[:scout_agent][:user]}"
+  when 'redhat','centos','fedora','scientific','suse','amazon'
+    "/var/spool/cron/#{node[:scout_agent][:user]}"
+  end
+
   bash "initialize scout" do
     code <<-EOH
     #{node[:scout_agent][:scout_bin]} #{node[:scout_agent][:key]}
     EOH
-    not_if do File.exist?("/var/spool/cron/crontabs/scout") end
+    not_if do File.exist?(crontab_path) end
   end
-  
+
   # schedule scout agent to run via cron
   cron "scout_run" do
     user node[:scout_agent][:user]
