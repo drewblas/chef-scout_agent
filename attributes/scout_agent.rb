9a4c3fa22da19e677b63335d7c5f2e701e92b215
Drew Blas <drew.blas@gmail.com>
2012-08-22 14:29:34 -0500

    Use default instead of set_unless Default to latest version

--- attributes/scout_agent.rb
+++ attributes/scout_agent.rb
@@ -1,4 +1,4 @@
-set_unless[:scout_agent][:user] = "scout"
-set_unless[:scout_agent][:group] = "scout"
-set_unless[:scout_agent][:version] = '5.0.3'
-set_unless[:scout_agent][:scout_bin]     = "/usr/bin/scout"
+default[:scout_agent][:user] = "scout"
+default[:scout_agent][:group] = "scout"
+default[:scout_agent][:version] = '5.5.6'
+default[:scout_agent][:scout_bin]     = "/usr/bin/scout"
