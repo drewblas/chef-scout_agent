80bc7b956774cd1ca6029015a7653e2e8908c764
Drew Blas <drew.blas@gmail.com>
2012-08-22 14:38:21 -0500

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-maintainer        "Seth Chisaore"
-maintainer_email  "schisamo@gmail.com"
+maintainer        "Drew Blas"
+maintainer_email  "drew.blas@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures the Scout Agent"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.1"
+version           "0.2"
 
-%w{ debian ubuntu}.each do |os|
+%w{debian ubuntu centos}.each do |os|
   supports os
 end
